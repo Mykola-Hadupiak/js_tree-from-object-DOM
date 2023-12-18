625dd088549131185783d52259f796c315b7a1fa
Mykola Hadupiak <mykola.hadupiak@gmail.com>
2023-12-18 23:24:31 +0200

    make build

---
+++ index.2a41ba43.js
@@ -0,0 +1,2 @@
+!function e(t,n){let r=document.createElement("ul");for(let l in t.append(r),n){let t=document.createElement("li");t.textContent=l,r.append(t),"object"==typeof n[l]&&e(r,n[l])}}(document.querySelector("#tree"),{Drink:{Wine:{},Schnaps:{}},Fruit:{Red:{Cherry:{},Strawberry:{}},Yellow:{Banana:{},Pineapple:{}}}});
+//# sourceMappingURL=index.2a41ba43.js.map
---
+++ index.f75de5e1.js
@@ -0,0 +1,31 @@
+"use strict";
+const food = {
+    "Drink": {
+        "Wine": {},
+        "Schnaps": {}
+    },
+    "Fruit": {
+        "Red": {
+            "Cherry": {},
+            "Strawberry": {}
+        },
+        "Yellow": {
+            "Banana": {},
+            "Pineapple": {}
+        }
+    }
+};
+const tree = document.querySelector("#tree");
+function createTree(element, data) {
+    const list = document.createElement("ul");
+    element.append(list);
+    for(const item in data){
+        const listItem = document.createElement("li");
+        listItem.textContent = item;
+        list.append(listItem);
+        if (typeof data[item] === "object") createTree(list, data[item]);
+    }
+}
+createTree(tree, food);
+
+//# sourceMappingURL=index.f75de5e1.js.map
